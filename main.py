343fd8db978bf8267f6876b5da1e68daf292b4fe
Марухленко Анатолий <proxy33@mail.ru>
2023-11-09 19:16:41 +0300

    1) added viewById, deleteById 2) method matrixTransponse 3) updated thunder collections

--- main.py
+++ main.py
@@ -11,14 +11,10 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlite.db'
 db = SQLAlchemy(app)
 
-@app.route("/")
-def hello():
-    return "hello"
-
 class WorkModel(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)    
     params = db.Column(JSON, nullable=False)
-    result = db.Column(JSON, nullable=False)    
+    result = db.Column(JSON)    
     createdat = db.Column(db.DateTime, default=datetime.datetime.now())
 
     @property
@@ -28,50 +24,68 @@ class WorkModel(db.Model):
             'params': self.params,
             'result': self.result,
         }
+    
 
-@app.route("/list",methods=['GET'])
-def getWorks():
-    # works = WorkModel.query.all()
-    works = WorkModel.query.order_by(WorkModel.id.desc()).limit(25)
-    return jsonify(works=[i.serialize for i in works])
-
-        
 def funcController(inJson):
     data = inJson['data'] 
-    value = data['value'] 
+    value = inJson['data']['value'] 
+
     match inJson['method']:
-        # Nice functions section
+        # Nice functions section ==========================================
         case "matrix-Julia":
             return funcNice.matrixReverse(value)
         case "ceasar":
             return funcNice.caesar(value,data['offset'])
         case "sum":
-            return funcNice.getSum(value)        
-        
-        # Usual functions section
+            return funcNice.getSum(value)   
+
+        # Usual functions section =========================================
         case "matrix-reverse":
             return func.matrixReverse(value)
         case "arraySum":
             return func.arraySum(value)
+        case "matrix-t":
+            return func.matrixTransponse(value)         
     return "method is not supported"
     
+@app.route("/")
+def hello():
+    return "hello"
+
+@app.route("/list",methods=['GET'])
+def getWorks():
+    works = WorkModel.query.order_by(WorkModel.id.desc()).limit(25)
+    return jsonify(works=[i.serialize for i in works])
 
 @app.route("/calc",methods=['POST'])
 def index():    
     try:
-        print(request.get_json())
         res = funcController(request.get_json())
         res =  {'data': {'result': res}}
-        #return {"data": res, }, 200
-        print(res)
         work = WorkModel(params=request.get_json(),result = res)
         db.session.add(work)
         db.session.commit()
         return {"data": work.serialize, }, 200
     except:
-        return {"data": 'wrong params', }, 400
+        return {"data": 'wrong json-params', }, 400
+    
+@app.route('/task/<id>', methods=['GET'])
+def viewTask(id):
+    return WorkModel.query.get(id).serialize
+
+@app.route('/task/<id>', methods=['DELETE'])
+def deleteTask(id):
+    s = "not found"
+    code = 404
+    if(WorkModel.query.get(id)):
+        db.session.delete(WorkModel.query.get(id))
+        db.session.commit()
+        code = 200 
+        s = "successfully deleted"       
+    return {"data": f"id={id} {s}"}, code
     
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     app.run(debug=True, port=2001)
+
--- utils/functions.py
+++ utils/functions.py
@@ -4,13 +4,15 @@ def matrixToString(M):
     s = '\n'.join([''.join([str(u) for u in row]) for row in M])    
     return (s.replace('[', '')).replace(']', '').replace('\n',';')
     
-def matrixReverse(data): # вычисляем обратную матрицу
+def matrixReverse(data): # обратная матрицу
     try:      
         return matrixToString((np.linalg.inv(np.matrix(data).T))) 
     except:
         return 'Fail'
 
-
-def arraySum(data): # вычисляем сумму элементов массива
+def arraySum(data): # сумма элементов массива
     M = data.split(' ')
     return np.sum(M,dtype=float)    
+
+def matrixTransponse(M): # транспонированная матрица
+    return matrixToString(np.matrix(M).T)
